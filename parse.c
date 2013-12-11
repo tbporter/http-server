92f10fca2f8737c422257dead8e6d5a4b6e63cf1
tbporter <travis.porter@gmail.com>
2013-12-11 02:33:33 -0500

    some untested parsing

---
+++ parse.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+#include "parse.h"
+
+#define BUF_LEN 512
+
+int parse_is_header_finished(char* buf, int buf_len){
+    int i;
+    for(i=0; i<buf_len-1; i++){
+        if(!strncmp(buf+i,"\r\n",2)){
+            return 1;
+        }
+    }
+    return 0;
+}
+
+void parse_header(char* buf, int buf_len){
+	if(!parse_is_header_finished(buf,buf_len)){
+		return;
+	}
+	char method[BUF_LEN], uri[BUF_LEN], ver[BUF_LEN], tmp[BUF_LEN];
+	char* str_s = buf;
+	int str_s_len;
+
+
+	str_s_len = parse_string(str_s, buf_len);
+	if(str_s_len<0)
+		return; //TODO: error!
+
+	strncpy(tmp, str_s, str_s_len);
+	sscanf(tmp, "%s %s %s\n", method, uri, ver);
+
+}
+
+int parse_string(char* start, int buf_len){
+	int i;
+	for(i=0; i<buf_len;i++){
+		if(start[i] == '\0')
+			return i;
+	}
+
+	return -1;
+}
---
+++ parse.h
@@ -0,0 +1,3 @@
+int parse_is_header_finished(char* buf, int buf_length);
+void parse_header(char* buf, int buf_length);
+int parse_string(char* start, int buf_len);
