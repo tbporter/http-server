3910dfff5639578d92900013d1bfdfe2e4a17361
Calvin Winkowski <telnoratti@gmail.com>
2013-12-11 23:53:53 -0500

    Switch to using epoll(2).

--- server.h
+++ server.h
@@ -1,13 +1,16 @@
 #ifndef _SERVER_H
 #define _SERVER_H
 #include <time.h>
+#include <sys/epoll.h>
 #include "list.h"
+#include "threadpool.h"
 int open_listenfd(int port);
 void* check_connections(void* data);
 
 struct http_socket {
     int fd;
-    struct pollfd* poll_fd;
+    /*struct pollfd* poll_fd;*/
+    struct epoll_event event;
     char* read_buffer;
     int read_buffer_size;
     time_t last_access;
