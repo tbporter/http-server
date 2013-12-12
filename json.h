8e8c38790d5a16941585338ed080a0830cf1e630
Calvin Winkowski <telnoratti@gmail.com>
2013-12-12 16:37:06 -0500

    loadavg_t struct to match /proc/loadavg.

--- json.h
+++ json.h
@@ -1,5 +1,14 @@
 #ifndef _JSON_H
 #define _JSON_H
+typedef struct {
+    float wait1;
+    float wait5;
+    float wait15;
+    int runable_entities;
+    int entities;
+    pid_t last_pid;
+} loadavg_t;
+
 int loadavg(struct buffer* buf);
 int meminfo(struct buffer* buf);
 
